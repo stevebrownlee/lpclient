c337b6a9f3bd40c91a467c574a7bd66b078e0b9f
Steve Brownlee <chortlehoort@gmail.com>
2020-04-26 12:01:01 -0500

    Some styling for profile

--- scripts/components/profile/Profile.js
+++ scripts/components/profile/Profile.js
@@ -1,5 +1,18 @@
 export const Profile = () => {
+    const profile = JSON.parse(localStorage.getItem("platform_profile"))
     return `
-        <h1>Profile</h1>
+        <div class="profile">
+            <h1 class="profile__header">${profile.user.name} Profile</h1>
+            <div>
+                <img class="profile__github profile__github--avatar" src="${profile.user.picture}" />
+            </div>
+            <div class="profile__github profile__github--username">
+                <a target="_blank" href="https://github.com/${profile.user.nickname}">${profile.user.nickname}</a>
+            </div>
+            <div class="profile__github profile__github--email">
+                ${profile.user.email}
+            </div>
+        </div>
+
     `
 }
